42b8937baf81f08436f3707d5cf182b526f407b6
nguyenvanson <sonnnvth1807031@fpt.edu.vn>
2018-09-30 02:16:42 +0700

    bai tap tao danh sach sinh vien

--- main.c
+++ main.c
@@ -1,21 +1,64 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 typedef struct {
-    char ten[50],ma_sinh_vien[5],phone[10];
-} Student;
-Student profiles[1];
-void luu_thong_tin_sv(int i, Student profiles[]) {
+    char ma_sinh_vien[10];
+    char ten_sinh_vien[50];
+    char so_dien_thoai[50];
+} danhsachsinhvien;
+
+void themsinhvien(int i, danhsachsinhvien sv[]) {
+    printf("Vui lòng nhập mã sinh viên:");
+    while (1 == 1) {
+        fgetc(stdin);
+        scanf("%s",sv[i].ma_sinh_vien);
+        if (strlen(sv[i].ma_sinh_vien) != 5) {
+            printf("Vui lòng nhập đủ 5 kí tự!\nVui lòng nhập lại:");
+        } else break;
+    }
+    fgetc(stdin);
+    printf("Vui lòng nhập tên sinh viên: ");
+    scanf("%[^\n]", sv[i].ten_sinh_vien);
+    fgetc(stdin);
+    printf("Vui lòng nhập số điện thoại sinh viên: ");
+    scanf("%s", sv[i].so_dien_thoai);
+    fgetc(stdin);
+}
+void indanhsachsinhvien(int dssv, danhsachsinhvien sv[]) {
+    int j;
+    printf("-%-20s|%-20s|%s", "Mã sinh viên", "Tên sinh viên", "Số điện thoại");
+    for (j = 0; j < dssv; j++) {
+        printf("\n-%-18s|%-18s|%s", sv[j].ma_sinh_vien, sv[j].ten_sinh_vien, sv[j].so_dien_thoai);
+    }
+}
+
+void luudanhsachsinhvien(int luu, danhsachsinhvien sv[]) {
     int j;
     FILE *fp = fopen("danhsachsinhvien.txt", "w");
     fprintf(fp, "-%-20s|%-20s|%s", "Mã sinh viên", "Tên sinh viên", "Số điện thoại");
-    for (j = 0; j < i; j++) {
-        fprintf(fp, "\n-%-18s|%-18s|%s", profiles[j].ma_sinh_vien, profiles[j].ten, profiles[j].phone);
+    for (j = 0; j < luu; j++) {
+        fprintf(fp, "\n-%-18s|%-18s|%s", sv[j].ma_sinh_vien, sv[j].ten_sinh_vien, sv[j].so_dien_thoai);
     }
     fclose(fp);
 }
-void doc_danh_sach() {
+void menu() {
+    printf("Vui lòng chọn từ menu bên dưới:\n");
+    printf("Phím 1.Thêm 1 sinh viên. \n");
+    printf("Phím 2.Hiển thị danh sách sinh viên. \n");
+    printf("Phím 3.Lưu danh sách sinh viên ra file. \n");
+    printf("Phím 4. Đọc danh sách sinh viên từ file.\n");
+    printf("Phím 5.Thoát chương trình. \n");
+}
+void menu2(){
+     printf("Phím 2.Hiển thị danh sách sinh viên. \n");
+     printf("Phím 3.Lưu danh sách sinh viên ra file. \n");
+     printf("Phím 4. Đọc danh sách sinh viên từ file.\n");
+     printf("Phím 5.Thoát chương trình. \n");
+
+}
+
+void docdssv() {
     FILE *f = fopen("danhsachsinhvien.txt", "r");
     int bufferSize = 225;
     char buffer[bufferSize];
@@ -23,61 +66,54 @@ void doc_danh_sach() {
         printf("%s", buffer);
     }
 }
-void main() {
-    Student profiles[1];
-    int lua_chon;
-    printf("Vui long nhap lua chon trong danh sach menu \n"
-           " Nhan phim 1: Them sinh vien \n "
-           "Nhan phim 2: Hien thi danh sach sv \n "
-           "Nhan phim 3: Luu danh sach sv ra file\n "
-           "Nhan phim 4: Doc danh sach sv tu file\n "
-           "Nhan phim 5: Thoat \n");
-    printf("Ban muon su dung:");
-    scanf("%d", &lua_chon);
-    if (lua_chon == 1) {
-        for (int i = 0; i < 1;i++) {
-            printf("Vui long nhap ma sinh vien %d:",i+1);
-            fgetc(stdin);
-            fgets(profiles[i].ma_sinh_vien,10, stdin);
-            if ((unsigned) strlen(profiles[i].ma_sinh_vien) != 5) {
-            } else {
-                printf("ma sinh vien sai, ma sinh vien phai co day du 5 ki tu\n");
-                printf("Vui long nhap lai ma sinh vien %d:",i+1);
-                fgetc(stdin);
-                scanf("%s", profiles[i].ma_sinh_vien);
-            }
-            printf("Vui long nhap ten cua sinh vien %d:",i+1);
-            fgetc(stdin);
-            scanf("%s", profiles[i].ten);
-            printf("Vui long nhap so dien thoai cua sinh vien %d:",i+1);
-            fgetc(stdin);
-            scanf("%s", profiles[i].phone);
+int main() {
+    int choice;
+    int i=0;
+    int max_sv = 10;
+    danhsachsinhvien sv[10];
+    menu();
+    do {
+        printf("\nVui lòng nhập lựa chọn của bạn: ");
+        scanf("%d", &choice);
+        switch (choice) {
+            case 1:
+                themsinhvien(i, sv);
+                i++;
+                if (i == max_sv ) {
+                     printf("\nVui lòng nhập lựa chọn của bạn: ");
+                     scanf("%d", &choice);
+
+                     switch (choice) {
+                         case 1:
+                             printf("danh sach da day");
+                             menu2();
+                             break;
+                     }
+                     if(choice == 5){
+                         printf("Thoát");
+                         break;
+                     }
+                }
+                break;
+            case 2:
+                printf("Danh sách sinh viên:\n");
+                indanhsachsinhvien (i, sv);
+                break;
+            case 3:
+                printf("Lưa vào file (danhsachsinhvien.txt) thành công!\n ");
+                luudanhsachsinhvien(i, sv);
+                break;
+            case 4:
+                printf("Danh sách sinh viên lấy từ file (danhsachsinhvien.txt): \n");
+                docdssv();
+                break;
+            case 5:
+                printf("Thoát!");
+                exit("exit");
+
         }
-        printf("danh sach sv da day\n");
-        printf("Vui long nhap lua chon tiep trong menu: \n"
-               " 2: Hien thi danh sach sv \n"
-               " 3: Luu danh sach sv ra file\n"
-               " 4: Doc danh sach sv tu file\n "
-               "5:Thoat :");
-        scanf("%d", &lua_chon);
     }
-    if (lua_chon == 2) {
-        printf("-%-20s|%-20s|%s", "Mã sinh viên", "Tên sinh viên", "Số điện thoại");
-        for (int j = 0; j < 1; j++) {
-            printf("\n-%-18s|%-18s|%s", profiles[j].ma_sinh_vien, profiles[j].ten, profiles[j].phone);
-        }
-    }
-    if (lua_chon == 3) {
-        int i =0;
-        printf("thong tin da duoc luu vao file (danhsachsinhvien.txt)\n");
-        luu_thong_tin_sv(i,profiles);
-    }
-    if (lua_chon == 4) {
-        printf("danh sach sinh vien trong file la:\n");
-        doc_danh_sach();
-    }
-    if (lua_chon == 5) {
-        printf("good bye! cam on da su dung dich vu cua chung toi!");
-        exit("thoat");
-    }
-};
+    while (max_sv = 10);
+
+
+}
